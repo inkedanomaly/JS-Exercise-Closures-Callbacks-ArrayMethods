1ee61cf5e8879db3fe9009cf9f91ada71eff7d8a
Melissa <inkedanomaly@gmail.com>
2020-01-28 19:59:34 -0800

    passed test 1, 2, 3

--- index.js
+++ index.js
@@ -68,8 +68,10 @@ function processLength(list, callback) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
-  /* CODE HERE */
+
+function processLastItem(stringList, callback) {  
+  return callback(stringList[stringList.length -1])
+
 }
 
 /**
@@ -89,9 +91,15 @@ function processLastItem(/* CODE HERE */) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
+
+
+function processSum(numberList, callback) {
+  const sum = numberList.reduce(function(accumulator, currentValue) {
+  return accumulator + currentValue;
+}, 0);
+  return callback(sum);
 }
+
 
 /**
  * ### Challenge `processProduct`
