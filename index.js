4340e29f000bd12458b23752737f699060e4ef57
Melissa <inkedanomaly@gmail.com>
2020-01-28 21:26:57 -0800

    passed processContains test

--- index.js
+++ index.js
@@ -145,9 +145,13 @@ function processProduct(num1, num2, callback) {
  * "lady gaga" and `['foo', 'bar']` and `(bool) => bool ? 'nice!' : 'sad'`,
  * should return "sad".
 */
-function processContains(/* CODE HERE */) {
-  /* CODE HERE */
+
+function processContains(item, list=['super','mega','rad'], callback) {
+  return callback(list.includes(item) , item ?  ``: ``);
 }
+
+
+
 
 /**
  * ### Challenge `processDuplicateFree`
